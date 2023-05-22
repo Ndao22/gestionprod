7faaebffbd727c5b97d9d4610b4b4d01f85bf966
Mariama Ndao <ndaomariama72@gmail.com>
2023-05-22 12:33:05 +0000

    Formulaire

---
+++ ajoutprod.php
@@ -0,0 +1,21 @@
+<div class="card col-md-8 offset-2 mt-5">
+
+    <div class="card-header bg-warning">Ajout produit</div>
+    <div class="card-body">
+        <form action="" method="post">
+            <label for="">Libelle</label>
+            <input type="text" class="form-control" name="libelle">
+            <br>
+            <label for="">Quantite</label>
+            <input type="text" class="form-control" name="qt">
+            <br>
+            <label for="">Prix Unitaire</label>
+            <input type="text" class="form-control" name="pu">
+            <br>
+            <button class="btn btn-danger" name="reset">Retour</button>
+            <button class="btn btn-success" name="submit">Ajouter</button>
+
+        </form>
+
+    </div>
+</div>
